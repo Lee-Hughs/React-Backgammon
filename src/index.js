7ba8468bbb8b38e3f81ce09edec7def2a9c58be4
unknown <hughs.stephenl@gmail.com>
2020-03-04 10:45:03 -0500

    front end backgammon game initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		let squares = [];
+		squares.push(["b",null,"b",null,"b",null,"b",null]);
+		squares.push([null,"b",null,"b",null,"b",null,"b"]);
+		squares.push(["b",null,"b",null,"b",null,"b",null]);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+		squares.push([null, null, null, null, null, null, null, null]);
+		squares.push([null, null, null, null, null, null, null, null]);
+		
+		squares.push([null,"r",null,"r",null,"r",null,"r"]);
+		squares.push(["r",null,"r",null,"r",null,"r",null]);
+		squares.push([null,"r",null,"r",null,"r",null,"r"]);
+
+		this.state= {
+			squares: squares,
+			stepNumber: 0,
+			player: 'Rr',
+			moveFrom: [-1,-1],
+			winner: null
+		};
+	}
+}
+
+
+
+// ===================
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById("root")
+);
+
